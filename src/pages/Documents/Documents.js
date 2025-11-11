99e6580552a065ef508a22d883fac6b08660cecd
ItzzHayden17 <sony.anray743@gmail.com>
2025-11-11 06:25:25 +0200

    changing documents

--- src/pages/Documents/Documents.js
+++ src/pages/Documents/Documents.js
@@ -5,14 +5,14 @@ const Documents = () => {
   return (
     <div className='Documents'>
         <a href='assets/documents/ComplaintsPolicy.pdf' download={"ComplaintsPolicy.pdf"}> Complaints Policy</a>
-        <a href='assets/documents/OutcomeOfRequestAndFeesPayable.pdf' download={"OutcomeOfRequestAndFeesPayable.pdf"}> OutcomeOfRequestAndFeesPayable</a>
-        <a href='assets/documents/RequestForCopyOfGuide.pdf' download={"RequestForCopyOfGuide.pdf"}> RequestForCopyOfGuide</a>
-        <a href='assets/documents/RequestForAccessToRecord.pdf' download={"RequestForAccessToRecord.pdf"}> RequestForAccessToRecord</a>
-        <a href='assets/documents/GeneralWebsiteTermsOfService.pdf' download={"GeneralWebsiteTermsOfService.pdf"}> GeneralWebsiteTermsOfService</a>
-        <a href='assets/documents/PAIANotice31August2025.pdf' download={"PAIANotice31August2025.pdf"}> PAIANotice31August2025</a>
+        <a href='assets/documents/PAIAFORM-3Outcomeofrequestandfeespayable.pdf' download={"PAIAFORM-3Outcomeofrequestandfeespayable.pdf"}> Outcome Of Request And Fees Payable</a>
+        <a href='assets/documents/PAIAFORM1-Requestforcopyofguide.pdf' download={"PAIAFORM1-Requestforcopyofguide.pdf"}> Request For Copy Of Guide</a>
+        <a href='assets/documents/PAIAFORM2-Requestforaccesstorecord.pdf' download={"PAIAFORM2-Requestforaccesstorecord.pdf"}> Request For Access To Record</a>
+        <a href='assets/documents/GeneralWebsiteTermsOfService.pdf' download={"GeneralWebsiteTermsOfService.pdf"}> General Website Terms Of Service</a>
+        <a href='assets/documents/PAIANotice31August2025.pdf' download={"PAIANotice31August2025.pdf"}> PAIA Notice 31 August 2025</a>
         <a href='assets/documents/PAIA-REQUEST-FORM.pdf' download={"PAIA-REQUEST-FORM.pdf"}> PAIA-REQUEST-FORM</a>
         <a href='assets/documents/POPI.pdf' download={"POPI.pdf"}> POPI</a>
-        <a href='assets/documents/WebsiteDisclaimer.pdf' download={"WebsiteDisclaimer.pdf"}> WebsiteDisclaimer</a>
+        <a href='assets/documents/WebsiteDisclaimer.pdf' download={"WebsiteDisclaimer.pdf"}> Website Disclaimer</a>
 
     </div>
   )
